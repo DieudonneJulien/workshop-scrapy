3e81eec5200ae7f49b8bb50b8d5d25916c2a57ed
Julien <julien.dieudonne@epitech.eu>
2022-02-16 18:44:54 +0100

    ADD settings.py

--- Part_2/my_scrap/my_scrap/settings.py
+++ Part_2/my_scrap/my_scrap/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'my_scrap.spiders'
 #USER_AGENT = 'my_scrap (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
